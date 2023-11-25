e9b5a4d77e22cd52f852eb21ae01e6ec895dff7c
peterkruhoffer <peter.kruhoffer@gmail.com>
2023-11-25 13:42:06 +0100

    Getting supabase setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,24 @@
+import { Session } from '@supabase/supabase-js';
 import { Link } from 'expo-router';
+import { useState, useEffect } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 
+import Account from '../components/Account';
+import Auth from '../components/Auth';
+import { supabase } from '../utils/supabase';
+
 export default function Page() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
   return (
     <View className={styles.container}>
       <View className={styles.main}>
@@ -9,7 +26,8 @@ export default function Page() {
           <Text className={styles.title}>Hello World</Text>
           <Text className={styles.subtitle}>This is the first page of your app.</Text>
         </View>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
+        {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+        <Link href={{ pathname: '/details', params: { name: 'Peter' } }} asChild>
           <TouchableOpacity className={styles.button}>
             <Text className={styles.buttonText}>Show Details</Text>
           </TouchableOpacity>
